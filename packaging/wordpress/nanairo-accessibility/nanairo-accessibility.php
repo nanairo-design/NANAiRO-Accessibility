b3749848563799b869c0a5b3fbd79c45def61609
Takuro Nakayama <t-nakayama@cloudot.co.jp>
2026-09-18 20:59:02 +0900

    Use association name for WordPress plugin author

--- packaging/wordpress/nanairo-accessibility/nanairo-accessibility.php
+++ packaging/wordpress/nanairo-accessibility/nanairo-accessibility.php
@@ -6,7 +6,7 @@
  * Version:           0.1.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Author:            NANAiRO
+ * Author:            一般社団法人ナナイロ
  * Author URI:        https://nanairo.design/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
